c56228788bc3c522ca08745f1a3a4bf03f6000e4
SaharGaiche23 <sahar.gaiche@esprit.tn>
2026-05-03 13:22:04 +0100

    fix: update CORS for plainte360-apii.onrender.com

--- backend/main.py
+++ backend/main.py
@@ -18,7 +18,7 @@ app = FastAPI(
 # CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:3000", "http://127.0.0.1:5173", "https://plainte360-app.netlify.app"],
+    allow_origins=["http://localhost:5173", "http://localhost:3000", "http://127.0.0.1:5173", "https://plainte360-app.netlify.app", "https://plainte360-apii.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
